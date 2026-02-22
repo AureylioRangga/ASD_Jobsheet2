c10b6837551031c88693fa8149a8809a31b4fd56
Aureylio <aureyliorangga2@gmail.com>
2026-02-22 17:20:26 +0700

    Latihan Praktikum no 2

--- DosenMain07.java
+++ DosenMain07.java
@@ -6,7 +6,7 @@ public class DosenMain07 {
         Dosen07 dsn1 = new Dosen07();
         dsn1.idDosen        = "D001";
         dsn1.nama           = "Dr. Budi Santoso";
-        dsn1.statusAktif    = true;
+        dsn1.statusAktif    = true; 
         dsn1.tahunBergabung = 2010;
         dsn1.bidangKeahlian = "Jaringan Komputer";
 
